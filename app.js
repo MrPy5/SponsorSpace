489f14bf5ccf7822b9bc2e410e096f94dabdd2af
MrPy5 <kalya.kothavale@gmail.com>
2023-11-24 16:06:22 -0600

    new

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+//https://www.youtube.com/watch?v=9kRgVxULbag
 document.addEventListener("DOMContentLoaded", event => {
 
     const app = firebase.app();
