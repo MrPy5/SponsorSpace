7afe573d5ef384ad05bd6437a85195f68f264999
MrPy5 <kalya.kothavale@gmail.com>
2023-11-29 19:08:58 -0600

    signout button

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 //Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
 //https://www.youtube.com/watch?v=9kRgVxULbag
+
 document.addEventListener("DOMContentLoaded", event => {
 
     const app = firebase.app();
     console.log(app);
+    
+    var loginButton = document.getElementById('login-button');
+    var nameDisplay = document.getElementById('name');
+    var signoutContainer = document.getElementById('signout-container');
+    if (localStorage.getItem("SessionUserEmail") == null) {
+        signoutContainer.style.display = "none";
+    }
+    else {
+        loginButton.style.display = "none";
+        nameDisplay.innerHTML = localStorage.getItem("SessionUserName");
+    }
 })
 
 function googleLogin() {
@@ -11,8 +23,14 @@ function googleLogin() {
 
     firebase.auth().signInWithPopup(provider).then(result => {
         const user = result.user;
-        document.write(user.displayName);
-        document.write(user.email);
+        localStorage.setItem("SessionUserEmail", user.email);
+        localStorage.setItem("SessionUserName", user.displayName);
+        window.location.replace("/index.html");
         console.log(user);
     })
+}
+
+function signout() {
+    localStorage.clear();
+    window.location.replace("/index.html");
 }
